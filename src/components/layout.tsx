18e744a614974556f37b1725bde7141a699f9de7
Joshua Mabry <inkandcapture@gmail.com>
2022-03-19 12:11:43 -0400

    Remove boilerplate code

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,9 +3,5 @@ import React from "react";
 export default function Layout({
   children,
 }: React.HTMLAttributes<HTMLElement>) {
-  return (
-    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
