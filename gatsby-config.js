9cb6fa39dab2f5d70976cd45e3f1ecffa3ca36c7
Joshua Mabry <inkandcapture@gmail.com>
2022-04-01 23:16:29 -0400

    Set html lang and robots plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,14 @@
 /** @type {import('gatsby').GatsbyConfig} */
+
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://www.joshuamabry.com",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === "production";
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
     title: `Joshua Mabry - Software Engineer`,
@@ -37,6 +47,27 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: "*" }],
+          },
+          "branch-deploy": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+          "deploy-preview": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
     },
   ],
 };
